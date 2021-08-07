2af08e9c61b9571ce8fe7b1ce3ce5c4492c298ce
daita2021 <daitamura2021@gmail.com>
2021-08-07 10:24:42 +0900

    コメント部分修正

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class FoodsController < ApplicationController
       def index
-        restaurant = Restaurant.find(params[:restaurant_id]) #params[:restaurant_id]は店舗IDを受け取る
+        restaurant = Restaurant.find(params[:restaurant_id]) #params[:restaurant_id]はリクエストに含まれている店舗IDを受け取る
         foods = restaurant.foods
 
         render json: {
