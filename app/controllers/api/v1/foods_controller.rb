e75a86f24a76b0f42f9a9b328a73a308b37934e3
daita2021 <daitamura2021@gmail.com>
2021-08-07 10:23:10 +0900

    jsonデータを返すapi foodsコントローラーの作成

---
+++ app/controllers/api/v1/foods_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    class FoodsController < ApplicationController
+      def index
+        restaurant = Restaurant.find(params[:restaurant_id]) #params[:restaurant_id]は店舗IDを受け取る
+        foods = restaurant.foods
+
+        render json: {
+          foods: foods
+        }, status: :ok
+      end
+    end
+  end
+end
